a06f70e0e05b59142b01cdd21d4736e6c12498a5
Pablo Martin <elektrochose@gmail.com>
2019-02-05 16:05:37 -0500

    added multihaed model

--- scripts/image_generator.py
+++ scripts/image_generator.py
@@ -1,48 +1,181 @@
 import os
+import re
+import hashlib
 import glob
 import random
+import collections
+import tensorflow as tf
+from tensorflow import Tensor
 
+class Inputs(object):
+	def __init__(self, bottlenecks, species_ground_truths, filenames):
+		self.bottlenecks = bottlenecks
+		self.species_ground_truths = species_ground_truths
+		self.filenames = filenames
+
+class ImageSplitter(object):
+	def __init__(self,
+				image_dir,
+				testing_percentage,
+				validation_percentage,
+				LONG_TAIL_CUTOFF):
+
+		self.image_dir = image_dir
+		self.testing_percentage = testing_percentage
+		self.validation_percentage = validation_percentage
+		self.LONG_TAIL_CUTOFF = LONG_TAIL_CUTOFF
+		self.image_lists = self.create_image_lists()
+		self.species_classes = len(self.image_lists.keys())
+		self.genus_classes = len(set([self.image_lists[w]['dir'].split('_')[0] \
+										for w in self.image_lists.keys()]))
+
+	def create_image_lists(self):
+		"""Builds a list of training images from the file system.
+
+		Analyzes the sub folders in the image directory, splits them into stable
+		training, testing, and validation sets, and returns a data structure
+		describing the lists of images for each label and their paths.
+
+		This function is adapted from Tensorflow:
+		curl -LO https://github.com/tensorflow/hub/raw/master/examples/image_retraining/retrain.py
+		"""
+		MAX_NUM_IMAGES_PER_CLASS = 2 ** 27 - 1
+		if not tf.gfile.Exists(self.image_dir):
+			tf.logging.error("Image directory '" + self.image_dir + "' not found.")
+			return None
+		result = collections.OrderedDict()
+		sub_dirs = sorted(x[0] for x in tf.gfile.Walk(self.image_dir))
+		# The root directory comes first, so skip it.
+		is_root_dir = True
+		for sub_dir in sub_dirs:
+			if is_root_dir:
+				is_root_dir = False
+				continue
+			extensions = sorted(set(os.path.normcase(ext)  # Smash case on Windows.
+									for ext in ['JPEG', 'JPG', 'jpeg', 'jpg', 'txt']))
+			file_list = []
+			dir_name = os.path.basename(sub_dir)
+			if dir_name == self.image_dir:
+				continue
+			tf.logging.info("Looking for images in '" + dir_name + "'")
+			for extension in extensions:
+				file_glob = os.path.join(self.image_dir, dir_name, '*.' + extension)
+				file_list.extend(tf.gfile.Glob(file_glob))
+			if not file_list:
+				tf.logging.warning('No files found')
+				continue
+			if len(file_list) < self.LONG_TAIL_CUTOFF:
+				tf.logging.warning(
+				  'WARNING: Folder has less than {} images, which may cause issues.'.format(len(file_list)))
+				long_tail = False
+			else:
+				long_tail = True
+
+			label_name = re.sub(r'[^a-z0-9]+', ' ', dir_name.lower())
+			training_images = []
+			testing_images = []
+			validation_images = []
+			for file_name in file_list:
+				base_name = os.path.basename(file_name)
+				# We want to ignore anything after '_nohash_' in the file name when
+				# deciding which set to put an image in, the data set creator has a way of
+				# grouping photos that are close variations of each other. For example
+				# this is used in the plant disease data set to group multiple pictures of
+				# the same leaf.
+				hash_name = re.sub(r'_nohash_.*$', '', file_name)
+				# This looks a bit magical, but we need to decide whether this file should
+				# go into the training, testing, or validation sets, and we want to keep
+				# existing files in the same set even if more files are subsequently
+				# added.
+				# To do that, we need a stable way of deciding based on just the file name
+				# itself, so we do a hash of that and then use that to generate a
+				# probability value that we use to assign it.
+				hash_name_hashed = hashlib.sha1(tf.compat.as_bytes(hash_name)).hexdigest()
+				percentage_hash = ((int(hash_name_hashed, 16) %
+								  (MAX_NUM_IMAGES_PER_CLASS + 1)) *
+								 (100.0 / MAX_NUM_IMAGES_PER_CLASS))
+				if percentage_hash < self.validation_percentage:
+					validation_images.append(base_name)
+				elif percentage_hash < (self.testing_percentage + self.validation_percentage):
+					testing_images.append(base_name)
+				else:
+					training_images.append(base_name)
+			result[label_name] = {
+				'dir': dir_name,
+				'long_tail' : long_tail,
+				'training': training_images,
+				'testing': testing_images,
+				'validation': validation_images,
+			}
+		return result
 
 class ImageGenerator(object):
-    def __init__(self,
-                 cutoff = 50,
-                 images_path = os.path.sep.join([os.environ['HOME'], 'flat_images'])):
-        self.cutoff = cutoff
-        self.all_fungi = self.generate_all_fungi_dict(images_path)
-        self.long_tail = self.generate_long_tail()
-        self.long = True
+	def __init__(self,
+				imagesplitter : ImageSplitter,
+				category,
+				balanced):
+
+		self.image_lists = imagesplitter.image_lists
+		self.image_dir = imagesplitter.image_dir
+		self.category = category
+		self.balanced = balanced
+		self.long_tail_species = \
+			[w for w in self.image_lists if self.image_lists[w]['long_tail']]
+		self.short_tail_species = \
+			[w for w in self.image_lists if not self.image_lists[w]['long_tail']]
+		self.long = True
+		self.Genus = self.build_genus()
+
+	def build_genus(self):
+		return list(set([self.image_lists[w]['dir'].split('_')[0] \
+											for w in self.image_lists.keys()]))
+
+	def get_next_image(self):
+		while True:
+			if not self.balanced:
+				# draw a species index at random
+				species_index = random.randrange(len(self.image_lists.keys()))
+				species_label = list(self.image_lists.keys())[species_index]
+			else:
+				if self.long:
+					species_label = random.choice(self.long_tail_species)
+					self.long = False
+				else:
+					species_label = random.choice(self.short_tail_species)
+					self.long = True
+				species_index = list(self.image_lists.keys()).index(species_label)
+
+			genus_label = self.image_lists[species_label]['dir'].split('_')[0]
+			genus_index = self.Genus.index(genus_label)
+			# draw a particular photo at random from right category
+			ex = random.choice(self.image_lists[species_label][self.category])
+
+			#get path and read image embedding "bottleneck"
+			bottleneck_path = os.path.join(self.image_dir, self.image_lists[species_label]['dir'], ex)
+
+			with open(bottleneck_path, 'r') as bottleneck_file:
+				bottleneck_string = bottleneck_file.read()
+			try:
+				bottleneck_values = [float(x) for x in bottleneck_string.split(',')]
+			except ValueError:
+				tf.logging.warning('Invalid float found, recreating bottleneck')
+			yield (bottleneck_values, genus_index, species_index, bottleneck_path)
 
 
-    def generate_all_fungi_dict(self, images_path):
-        # generates a dictionary between a person and all the photos of that person
-        all_fungi = {}
-        for species_folder in os.listdir(images_path):
-            species_photos = glob.glob(os.path.sep.join([images_path, species_folder , '*.JPG']))
-            all_fungi[species_folder] = species_photos
-        return all_fungi
+class Dataset(object):
+	def __init__(self, generator : ImageGenerator,
+					  batch_size = 100,
+					  prefetch_batch_buffer = 50,
+					  balanced = 0):
+		self.batch_size = batch_size
+		self.prefetch_batch_buffer = prefetch_batch_buffer
+		self.next_element = self.build_iterator(generator.get_next_image)
 
-    def generate_long_tail(self):
-        lt = {k:len(v) for k,v in self.all_fungi.iteritems()}
-        lt = {k: v>self.cutoff for k,v in lt.items()}
-        tail = {}
-        tail['long'] = [k for k,v in lt.items() if v == True]
-        tail['short'] = [k for k,v in lt.items() if v == False]
-        return tail
+	def build_iterator(self, generator_func):
 
-    def get_next_image_random(self):
-        while True:
-            # draw a species at random
-            species = random.choice(self.all_fungi.keys())
-            # draw a particular photo at random
-            yield (random.choice(self.all_fungi[species]))
-
-    def get_next_image_balanced(self):
-        while True:
-            if self.long:
-                species = random.choice(self.long_tail['long'])
-                self.long = False
-            else:
-                species = random.choice(self.long_tail['short'])
-                self.long = True
-            #picking photo at random from within that species
-            yield (random.choice(self.all_fungi[species]))
+		dataset = tf.data.Dataset.from_generator(generator_func,
+								output_types=(tf.float32, tf.int64, tf.int64, tf.string))
+		dataset = dataset.batch(self.batch_size)
+		dataset = dataset.prefetch(self.prefetch_batch_buffer)
+		iter = dataset.make_one_shot_iterator()
+		return iter.get_next()
--- scripts/main.py
+++ scripts/main.py
@@ -1,138 +1,139 @@
+import os
 import sys
 import argparse
 import tensorflow as tf
-def main(_):
-    print('main')
-    print FLAGS
+
+from scripts.separate_imgs import separate_images_from_config
+from scripts.model import MultiHead_Model
+from scripts.image_generator import ImageSplitter, ImageGenerator, Dataset
+
+def main():
+
+    config = configparser.ConfigParser()
+    config.read('/home/ubuntu/Mushroom-Hikes/scripts/defaults.config')
+    '''
+    this will separate images before creating bottlenecks - not as important
+    now that they have been precomputed
+    '''
+    separate_images_from_config(config)
+
+    '''
+    Values loaded from config file
+    '''
+    bottleneck_dir = config['files']['BOTTLENECK_DIR']
+    testing_percentage = config['data']['testing_percentage']
+    validation_percentage = config['data']['validation_percentage']
+    LONG_TAIL_CUTOFF = config['data']['LONG_TAIL_CUTOFF']
+    #category
+    balanced = config['training']['balanced']
+    batch_size = config['training']['batch_size']
+    prefetch_batch_buffer = config['training']['prefetch_batch_buffer']
+    TRAINING_STEPS = config['training']['TRAINING_STEPS']
+    EVALUATION_STEP = config['training']['EVALUATION_STEP']
+    learning_rate = config['model']['learning_rate']
+
+    '''
+    Example values for debugging
+    '''
+    bottleneck_dir = '/Users/pablomartin/python/Mushroom_Classifier/bottlenecks'
+    testing_percentage = 10
+    validation_percentage = 10
+    LONG_TAIL_CUTOFF = 150
+    category = 'training'
+    balanced = 0
+    batch_size = 100
+    prefetch_batch_buffer = 50
+    learning_rate = 0.01
+    TRAINING_STEPS = 10
+    EVALUATION_STEP = 5
+
+    #we must split dataset first, and feed that into different generators
+    Imagelists = ImageSplitter(bottleneck_dir,
+                               testing_percentage,
+                               validation_percentage,
+                               LONG_TAIL_CUTOFF)
+
+
+    #gives us a random image file from the training set
+    training_gen = ImageGenerator(imagesplitter = Imagelists,
+                                  category = 'training',
+                                  balanced = balanced)
+    #gives us a random image file from the validation set
+    validation_gen = ImageGenerator(imagesplitter = Imagelists,
+                                    category = 'validation',
+                                    balanced = balanced)
+    #ditto for testing set
+    testing_gen = ImageGenerator(imagesplitter = Imagelists,
+                                    category = 'testing',
+                                    balanced = balanced)
+
+    #training tensorflow dataset
+    training_dataset = Dataset(generator = training_gen,
+                                batch_size = batch_size,
+                                prefetch_batch_buffer = prefetch_batch_buffer,
+                                balanced = balanced)
+    #validation tensorflow dataset
+    validation_dataset = Dataset(generator = validation_gen,
+                                batch_size = batch_size,
+                                prefetch_batch_buffer = prefetch_batch_buffer,
+                                balanced = balanced)
+
+    training_dataset = Dataset(generator = testing_gen,
+                                batch_size = batch_size,
+                                prefetch_batch_buffer = prefetch_batch_buffer,
+                                balanced = balanced)
+
+    #let's load our model
+    SPECIES_COUNT = Imagelists.species_classes
+    GENUS_COUNT = Imagelists.genus_classes
+    M = MultiHead_Model(shared_layer_size = 1000,
+                        genus_count = GENUS_COUNT,
+                        species_count = SPECIES_COUNT,
+                        is_training = 1,
+                        learning_rate =  0.01)
+
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+
+        for unused_i in range(TRAINING_STEPS):
+            bottlenecks, genus_truths, species_truths, _ = \
+                                        sess.run(training_dataset.next_element)
+            #training step
+            sess.run(M.train_step, feed_dict = \
+                                {M.bottleneck_input : bottlenecks,
+                                 M.ground_truth_species_input : species_truths,
+                                 M.ground_truth_genus_input: genus_truths,
+                                 M.keep_prob : 0.5})
+
+            if TRAINING_STEPS % EVALUATION_STEP == 0:
+                '''
+                We are evaluating accuracy on the training batch that we just
+                trained on
+                '''
+                #evaluation step
+                genus_accuracy, species_accuracy = \
+                          sess.run([M.genus_evaluation_step,
+                                    M.species_evaluation_step], feed_dict = \
+                                    {M.bottleneck_input : bottlenecks,
+                                     M.ground_truth_species_input : species_truths,
+                                     M.ground_truth_genus_input: genus_truths,
+                                     M.keep_prob : 1.0})
+                '''
+                Now we are drawing validation_batch_size pictures from the
+                validation set and calculating accuracy, and NOT changing the
+                weights of the network
+                '''
+
+                bottlenecks, genus_truths, species_truths, _ = \
+                                        sess.run(validation_dataset.next_element)
+                #evaluation step
+                genus_accuracy, species_accuracy = \
+                          sess.run([M.genus_evaluation_step,
+                                    M.species_evaluation_step], feed_dict = \
+                                    {M.bottleneck_input : bottlenecks,
+                                     M.ground_truth_species_input : species_truths,
+                                     M.ground_truth_genus_input: genus_truths,
+                                     M.keep_prob : 1.0})
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-                        description = "Train Mushroom Classification CNN-RNN")
-    parser.add_argument(
-      '--image_dir',
-      type=str,
-      default='',
-      help='Path to folders of labeled images.'
-    )
-    parser.add_argument(
-      '--output_graph',
-      type=str,
-      default='/tmp/output_graph.pb',
-      help='Where to save the trained graph.'
-    )
-    parser.add_argument(
-      '--intermediate_output_graphs_dir',
-      type=str,
-      default='/tmp/intermediate_graph/',
-      help='Where to save the intermediate graphs.'
-    )
-    parser.add_argument(
-      '--intermediate_store_frequency',
-      type=int,
-      default=0,
-      help="""\
-         How many steps to store intermediate graph. If "0" then will not
-         store.\
-      """
-    )
-    parser.add_argument(
-      '--output_labels',
-      type=str,
-      default='/tmp/output_labels.txt',
-      help='Where to save the trained graph\'s labels.'
-    )
-    parser.add_argument(
-      '--summaries_dir',
-      type=str,
-      default='/tmp/retrain_logs',
-      help='Where to save summary logs for TensorBoard.'
-    )
-    parser.add_argument(
-      '--how_many_training_steps',
-      type=int,
-      default=4000,
-      help='How many training steps to run before ending.'
-    )
-    parser.add_argument(
-      '--learning_rate',
-      type=float,
-      default=0.01,
-      help='How large a learning rate to use when training.'
-    )
-    parser.add_argument(
-      '--testing_percentage',
-      type=int,
-      default=10,
-      help='What percentage of images to use as a test set.'
-    )
-    parser.add_argument(
-      '--validation_percentage',
-      type=int,
-      default=10,
-      help='What percentage of images to use as a validation set.'
-    )
-    parser.add_argument(
-      '--eval_step_interval',
-      type=int,
-      default=10,
-      help='How often to evaluate the training results.'
-    )
-    parser.add_argument(
-      '--train_batch_size',
-      type=int,
-      default=100,
-      help='How many images to train on at a time.'
-    )
-    parser.add_argument(
-      '--test_batch_size',
-      type=int,
-      default=-1,
-      help="""\
-      How many images to test on. This test set is only used once, to evaluate
-      the final accuracy of the model after training completes.
-      A value of -1 causes the entire test set to be used, which leads to more
-      stable results across runs.\
-      """
-    )
-    parser.add_argument(
-      '--validation_batch_size',
-      type=int,
-      default=100,
-      help="""\
-      How many images to use in an evaluation batch. This validation set is
-      used much more often than the test set, and is an early indicator of how
-      accurate the model is during training.
-      A value of -1 causes the entire validation set to be used, which leads to
-      more stable results across training iterations, but may be slower on large
-      training sets.\
-      """
-    )
-    parser.add_argument(
-      '--print_misclassified_test_images',
-      default=False,
-      help="""\
-      Whether to print out a list of all misclassified test images.\
-      """,
-      action='store_true'
-    )
-    parser.add_argument(
-      '--bottleneck_dir',
-      type=str,
-      default='/tmp/bottleneck',
-      help='Path to cache bottleneck layer values as files.'
-    )
-    parser.add_argument(
-      '--final_tensor_name',
-      type=str,
-      default='final_result',
-      help="""\
-      The name of the output classification layer in the retrained graph.\
-      """
-    )
-    parser.add_argument(
-      '--saved_model_dir',
-      type=str,
-      default='',
-      help='Where to save the exported graph.')
-    FLAGS, unparsed = parser.parse_known_args()
-    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    main()
